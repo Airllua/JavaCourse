b4a2e8485807ceb4641aac3ead27fad9faaa022a
Airllua <nekitaimer@gmail.com>
2023-10-18 18:31:37 +0300

    initial commit

---
+++ src/extraTask_1/Employee.java
@@ -0,0 +1,22 @@
+package extraTask_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private ArrayList<EmployeeSpecialization> specialization = new ArrayList<>();
+    private double salary;
+
+    Employee (EmployeeSpecialization[] specialization, double salary) {
+        this.specialization.addAll(List.of(specialization));
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public ArrayList<EmployeeSpecialization> getSpecialization() {
+        return specialization;
+    }
+}
